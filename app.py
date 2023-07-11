3a10e6eeb7fdc3428557d0445cc4f253a70254e7
Kunal Agrawal <55643948+agrawalka@users.noreply.github.com>
2023-07-11 13:06:17 +0530

    Update app.py
    added range of years rather than selecting single year.

--- app.py
+++ app.py
@@ -1,32 +1,49 @@
-from flask import Flask,render_template,request,jsonify
+from flask import Flask, render_template, request, jsonify
 import pandas as pd
-
+import sqlite3 as sql
 import csv
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
+
 @app.route("/recommender",methods=["POST"])
 def recommender():
     if request.method == "POST":
         genre = request.form["genre"]
-        year = request.form["year"]
+        selected_range = request.form["year"]
 
-        # opening the dataset and finding the movies with specefic genres and year enterd by user
-        with open("movie_metadata.csv","r", encoding="utf8") as cv:
-            csv_rdr=csv.reader(cv)
-            next(csv_rdr)
+        if selected_range == "1900-1950":
+            year_range = range(1900, 1951)
+        elif selected_range == "1950-2000":
+            year_range = range(1950, 2001)
+        elif selected_range == "2000-2023":
+            year_range = range(2000, 2024)
+        else:
+            year_range = []
+        
+                
+        conn = sql.connect("movies_db.db")
 
-            filtered_movies=[]
+        c = conn.cursor()
+        
+        c.execute('''
+            SELECT * FROM movies 
+            WHERE genres LIKE ? AND title_year BETWEEN ? AND ?
+            ORDER BY imdb_score DESC
+        ''', ('%' + genre + '%', min(year_range), max(year_range)))
+        
 
-            for row in csv_rdr:
-                if genre in row[4] and year == row[12] :
-                    filtered_movies.append(row)
+        filtered_movies = c.fetchall()
+        
 
-        return render_template('result.html', movies=filtered_movies)
+        return render_template('result.html', movies=filtered_movies, genre=genre, selected_range=selected_range)
+
+
 
 @app.route("/about")
 def about():
@@ -42,7 +59,3 @@ def joinus():
 
 if __name__ == "__main__":
     app.run(debug=True  )
-
-
-
-
